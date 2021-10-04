4569b4a4a3f4e375ef2441e3defa29928274b0a5
dawidkaszuba <dawidk89@gmail.com>
2021-10-04 22:25:45 +0200

    initial commit

---
+++ src/test/java/pl/dawidkaszuba/registry/service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dawidkaszuba.registry.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
